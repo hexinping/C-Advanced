c15184d5655e2391c991330578eb637adba52a4e
hexinping <hexinping456@126.com>
2021-06-12 21:32:22 +0800

    c# project new

---
+++ Advance/Advance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hexinping");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
