3ece3a33e8dc4129972009ecc1fd80b51b436fe9
hexinping <hexinping456@126.com>
2021-07-22 21:42:33 +0800

    Update

--- Advance/Advance/Program.cs
+++ Advance/Advance/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,11 +15,29 @@ namespace Advance
             List<int> list = new List<int>(100);
             list.Add(1);
 
-            string str = "sdfsdf";
-            int a = str.Length;
+  
+        
+            
+            
+            Task<int> taskAsync= GetPageLengthAsync("https://www.baidu.com/");
+            Console.WriteLine($"Length is {taskAsync.Result}");  //taskAsync需要解包下
+            
+         
+            
+            
+            
+            
             Console.ReadLine();
+        }
 
-
+        static async Task<int> GetPageLengthAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> f = client.GetStringAsync(url);
+                int length = (await f).Length;
+                return length;  //虽然返回int，但是函数声明返回值为Task<int> 会自动进行包装，
+            }
         }
     }
 }
